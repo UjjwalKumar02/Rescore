083ae3e5209f3c9f3e58fdc3f8c2a4a7b7cce689
UjjwalKumar02 <kumarujjwaldos@gmail.com>
2025-09-27 20:17:05 +0530

    Update Landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const poppins = Poppins({
   variable: '--font-poppins',
   subsets: ['latin'],
-  weight: ['400', '700']
+  weight: ['400', '500', '700']
 })
 
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 "use client";
 
+
 import Link from "next/link";
 import { useState } from "react";
+import Image from 'next/image';
+import img from "../public/app_screenshot.jpeg"
+import { RiOpenSourceLine } from "react-icons/ri";
 
 export default function Home() {
   const [loading, setLoading] = useState(false);
@@ -12,11 +16,36 @@ export default function Home() {
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center gap-6 px-2 text-center">
-      <h1 className="text-3xl">Resume score</h1>
-      <p>Don&apos;t you wanna know about your resume...</p>
-      <Link href={"/score-prediction"} onClick={handleClick} className="bg-black text-white px-9 py-1.5 rounded text-sm">
-        {loading ? "Loading..." : "Get Started"}
-      </Link>
+      <div className="w-full fixed top-0 flex justify-between px-12 py-5 items-center">
+        <p className="text-2xl font-medium font-sans">Rescore</p>
+        <div>
+          <a href="https://github.com/UjjwalKumar02/Resume-score" className="bg-[#4d1cff] text-white px-6 py-1.5 rounded-lg text-sm">Github</a>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center justify-center gap-5 px-2 text-center mt-27 mb-8">
+        <div className="border border-gray-500 py-1.5 px-5 rounded-full text-sm flex items-center gap-1 ">
+          <RiOpenSourceLine size={20} color="blue" />
+          Open source
+        </div>
+        <div className="space-y-3">
+          <p className="text-4xl leading-[1.4] font-medium ">
+            Real-Time Resume
+            <br />
+            Evaluation based on Job Descriptions
+          </p>
+          <p>
+            Predicts candidate fit score and ranking
+          </p>
+        </div>
+        <Link href={"/score-prediction"} onClick={handleClick} className="border border-gray-300 bg-[#4d1cff] text-white px-8 py-1.5 rounded-lg hover:bg-[#3414b4]">
+          {loading ? "Loading..." : "Get Started >"}
+        </Link>
+      </div>
+
+      <div className="border-t-9 border-x-9 rounded-t-3xl p-2">
+        <Image src={img} alt="image" height={100} width={960} className="rounded-3xl" />
+      </div>
     </div>
   );
 }
